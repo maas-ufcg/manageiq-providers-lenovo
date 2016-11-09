88c07bd840d9c70a9a2e37b9e3229dd35b22e6e8
Tercio de Melo <terciodemelo@gmail.com>
2016-11-09 16:46:43 -0300

    Changes Worker's hierarchy

--- app/models/manageiq/providers/lenovo/physical_infra_manager/refresh_worker.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager/refresh_worker.rb
@@ -1,4 +1,4 @@
-class ManageIQ::Providers::Lenovo::PhysicalInfraManager::RefreshWorker < ::MiqEmsRefreshWorker
+class ManageIQ::Providers::Lenovo::PhysicalInfraManager::RefreshWorker < ManageIQ::Providers::BaseManager::RefreshWorker
   require_nested :Runner
 
   def self.ems_class
